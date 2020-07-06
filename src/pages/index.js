49438cfb0a37db7af8d39a03254c2c690d0b9b70
prabakarviji <prabakarviji@gmail.com>
2020-07-06 17:21:12 +0530

    Icons added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,12 @@ import Layout from "../components/layout"
 import Logo from "../images/avatar.png"
 import SEO from "../components/seo"
 import Styles from "./index.module.css"
+import {
+  Github,
+  Medium,
+  Twitter,
+  Instagram,
+} from "@styled-icons/boxicons-logos"
 
 const IndexPage = () => (
   <Layout>
@@ -13,10 +19,27 @@ const IndexPage = () => (
       </span>
       <h1 className={Styles.title}>Prabakaran Marimuthu</h1>
       <img src={Logo} alt="Logo" className={Styles.avatar} />
+      <span className={Styles.subTitle}>
+        Senior Software Engineer @ Spritle Software
+      </span>
       <p className={Styles.desc}>
         I'm an avid fan of Tesla and Iron Man. I strongly believe that great
-        things resulting from hard work
+        things resulting from hard work.
       </p>
+      <div>
+        <a href={"https://github.com/prabakarviji"}>
+          <Github size="40" className={Styles.icons} />
+        </a>
+        <a href={"https://twitter.com/prabarth_m"}>
+          <Twitter size="40" className={Styles.icons} />
+        </a>
+        <a href={"https://medium.com/@prabart"}>
+          <Medium size="40" className={Styles.icons} />
+        </a>
+        <a href={"http://instagram.com/prabakviji"}>
+          <Instagram size="40" className={Styles.icons} />
+        </a>
+      </div>
     </div>
   </Layout>
 )
