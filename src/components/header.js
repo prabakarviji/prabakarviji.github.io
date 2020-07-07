081fd2346aac5bc022e66bca2af586bc0c2e5a96
prabakarviji <prabakarviji@gmail.com>
2020-07-07 19:06:09 +0530

    Header style moved to stylesheet

--- src/components/header.js
+++ src/components/header.js
@@ -1,81 +1,25 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Styles from "./header.module.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `white`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        display: "flex",
-        flexDirection: "row",
-      }}
-    >
+  <header className={Styles.header}>
+    <div className={Styles.siteTitle}>
       <div style={{ margin: 0, flex: 0.8 }}>
-        <Link
-          to="/"
-          style={{
-            color: `black`,
-            textDecoration: `none`,
-            fontSize: `1rem`,
-            fontWeight: 550,
-          }}
-        >
+        <Link to="/" className={Styles.siteName}>
           {siteTitle}
         </Link>
       </div>
 
-      <div
-        style={{
-          margin: 0,
-          flex: 0.2,
-          display: "flex",
-          justifyContent: "flex-end",
-          alignItems: "center",
-        }}
-      >
-        <Link
-          to="/"
-          style={{
-            color: `black`,
-            textDecoration: `none`,
-            fontSize: `0.9rem`,
-            fontWeight: 500,
-            paddingRight: 10,
-          }}
-        >
+      <div className={Styles.menuBlock}>
+        <Link to="/" className={Styles.menu}>
           Home
         </Link>
-        <Link
-          to="/"
-          style={{
-            color: `black`,
-            textDecoration: `none`,
-            fontSize: `0.9rem`,
-            fontWeight: 500,
-            paddingLeft: 10,
-            paddingRight: 10,
-          }}
-        >
+        <Link to="/" className={Styles.menu}>
           Blog
         </Link>
-        <Link
-          to="/"
-          style={{
-            color: `black`,
-            textDecoration: `none`,
-            fontSize: `0.9rem`,
-            fontWeight: 500,
-            paddingLeft: 10,
-          }}
-        >
+        <Link to="/" className={Styles.menu}>
           D/N
         </Link>
       </div>
